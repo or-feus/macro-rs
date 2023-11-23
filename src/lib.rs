2b0f62faedf186659ecb8cbf852157aaf0c8682a
nFeus <barca105@nate.com>
2023-11-23 14:35:08 +0900

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+
+trait HelloMacro {
+    fn hello_macro();
+}
---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use hello_macro::HelloMacro;
+use macro_rs::HelloMacro;
+
+struct Pancakes;
+
+impl HelloMacro for Pancakes {
+    fn hello_macro() {
+        println!("Hello, Macro! My name is Pancakes!");
+    }
+}
+
+
+fn main() {
+    Pancakes::hello_macro();
+}
